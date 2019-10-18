3202a1f028788798144ff50891d58ad9206e6a37
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-10-18 14:11:39 +0200

    feat: Add input for wallet

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,11 +2,31 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class Wallet extends Component {
-  state = {};
+  state = {
+    amount: 0
+  };
   render() {
     const { balance } = this.props;
 
-    return <div className="balance">Wallet Balance: {balance}</div>;
+    return (
+      <div className="wallet">
+        <div className="balance">
+          <p>Wallet Balance: {balance}</p>
+        </div>
+        <hr />
+        <div>
+          <form>
+            <label htmlFor="amount">Enter amount</label>
+            <input
+              type="text"
+              name="amount"
+              className="wallet-input"
+              onChange={({ target }) => this.setState({ amount: parseInt(target.value, 10) })}
+            />
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -11,7 +11,28 @@ describe("Wallet", () => {
     expect(wallet).toMatchSnapshot();
   });
 
+  it("should initialize the `state` to 0", () => {
+    expect(wallet.state().amount).toEqual(0);
+  });
+
   it("displays the balance from props", () => {
-    expect(wallet.find(".balance").text()).toEqual("Wallet Balance: 20");
+    expect(wallet.find(".balance > p").text()).toEqual("Wallet Balance: 20");
+  });
+
+  it("finds the Wallet input", () => {
+    expect(wallet.find(".wallet-input").exists()).toBe(true);
+  });
+
+  describe("when the user types into the wallet input", () => {
+    const AMOUNT = "10";
+
+    beforeEach(() => {
+      wallet.find(".wallet-input").simulate("change", { target: { value: AMOUNT } });
+    });
+
+    it("should set the `state` to the amount and convert it to a number", () => {
+      expect(wallet.state().amount).toEqual(parseInt(AMOUNT, 10));
+      //expect(wallet.state().amount).toBe();
+    });
   });
 });
