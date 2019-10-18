1d47e0cd4bef2428e4a83d718d3ae4dd7f4dbbb8
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-10-18 10:52:05 +0200

    feat: Add deposit and withdraw

--- src/actions/balance.js
+++ src/actions/balance.js
@@ -6,3 +6,17 @@ export function setBalance(balance) {
     balance
   };
 }
+
+export function deposit(deposit) {
+  return {
+    type: constants.DEPOSIT,
+    deposit
+  };
+}
+
+export function withdraw(withdrawal) {
+  return {
+    type: constants.WITHDRAW,
+    withdrawal
+  };
+}
